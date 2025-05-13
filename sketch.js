e919361acedbed965aea5ece8bd756568cc1ea0b
Xiao-Binzhi <xbz462693964@gmail.com>
2025-05-13 14:29:51 +0200

    123

--- sketch.js
+++ sketch.js
@@ -1,51 +1,67 @@
-const testo = "WTF";
-const fontSize = 120;
-const parti = 40;
-const sfasamento = 4;
+const testo = "BILIBALA";
+const font_size = 100;
+const percorso_font = "./fonts/Outfit-SemiBold.ttf";
+const lunghezza_linea = 5;
+const densita = 0.25;
+const griglia = 20
 
 //
 
 /** @type {Font} */
 let font;
 
+let palette = [
+  '#1B75BB','#F15A24','#FBB03B','#8DC63F','#662D91'
+];
+
 function preload() {
-  font = loadFont("./fonts/Adobe-Jenson-Pro-Bold-Caption.ttf");
+  font = loadFont(percorso_font);
 }
 
+//
+
 function setup() {
-  createCanvas(400, 400, "svg");
+  createCanvas(windowWidth, windowHeight, "svg");
   addDownloadButton();
 
-  rectMode(CENTER);
-  angleMode(DEGREES);
-
-  noLoop(); // Opzionale
+  rectMode(CORNER);
+  noLoop(); // 只渲染一次
+  noStroke()
+  textFont(font)
+  textSize(font_size)
 }
 
 function draw() {
   clear(); // Non cancellare!
 
-  textFont(font);
-  textLeading(fontSize);
-  textSize(fontSize);
+  background('#F3E9D2');
 
-  const text_width = textWidth(testo);
-  const h_parti = fontSize / parti;
+  
 
-  noStroke();
-  fill(0);
+   // 列数、行数
+   const cols = ceil(width  / griglia);
+   const rows = ceil(height / griglia);
 
-  for (let i = 0; i < parti; i++) {
-    push();
-    translate(text_width / 2, 0);
-    translate(random(-sfasamento, sfasamento), 0);
-
-    beginClip();
-    rect(0, i * h_parti + h_parti / 2, text_width, h_parti);
-    endClip();
-
-    textSVG(testo, -text_width / 2, fontSize);
-
-    pop();
+   for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if(random() < densita) {
+        // 宽高随机：1~3 格
+        let w = int(random(1, 4)) * griglia;
+        let h = int(random(1, 4)) * griglia;
+        let x = i * griglia;
+        let y = j * griglia;
+        fill(random(palette));
+        rect(x, y, w, h);
+    }
   }
+}
+
+  fill(0)
+  text(testo, width/2, height/4)
+}   
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    redraw();
+
 }
